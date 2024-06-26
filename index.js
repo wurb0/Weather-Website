adef9a6326d7a11914c98ac55cf5b462b2849031
Mustafa Saqib <xvp605@usask.ca>
2024-06-26 18:53:04 +0400

    prod

--- index.js
+++ index.js
@@ -5,12 +5,43 @@
 const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?appid=5fdbba21ce9ccd27ffa8392103285bf1&units=metric&q=";
 
-async function checkWeather() {
-  var city = "cairo";
+document
+  .querySelector("#search input")
+  .addEventListener("keypress", function (event) {
+    // Check if the key pressed was Enter (key code 13)
+    if (event.keyCode === 13) {
+      // Call the checkWeather function with the input field's value
+      checkWeather(this.value);
+    }
+  });
+
+const icon = document.querySelector("#weather-icon");
+
+async function checkWeather(city) {
   const repsonse = await fetch(apiUrl + city);
   var data = await repsonse.json();
 
   console.log(data);
+
+  document.querySelector("#city").innerHTML = data.name;
+  document.querySelector("#temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector("#humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector("#wind").innerHTML = data.wind.speed + " km/h";
+
+  console.log(temp);
+  console.log(humidity);
+
+  if (data.weather[0].main == "Clouds") {
+    icon.src = "images/clouds.png";
+  } else if (data.weather[0] == "Clear") {
+    icon.src = "images/clear.png";
+  } else if (data.weather[0] == "Rain") {
+    icon.src = "images/rain.png";
+  } else if (data.weather[0] == "Drizzle") {
+    icon.src = "images/drizzle.png";
+  } else if (data.weather[0] == "Mist") {
+    icon.src = "images/mist.png";
+  }
 }
 
-checkWeather();
+checkWeather("dubai");
